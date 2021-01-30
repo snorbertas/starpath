da203108c2c29429b44094e717aa6fa820efad22
Norb <snorbertas@gmail.com>
2021-01-31 07:50:09 +1300

    main menu hotfix

--- Assets/_Jam/Scripts/UI/MenuController.cs
+++ Assets/_Jam/Scripts/UI/MenuController.cs
@@ -7,9 +7,20 @@ public class MenuController : MonoBehaviour
 {
     public static bool LoadedFromMenu = false;
     public string gameplayScene = "norb3";
+    private bool loading = false;
     public void PressedPlay()
     {
+        if (loading) return;
+        loading = true;
         LoadedFromMenu = true;
         SceneManager.LoadScene(gameplayScene);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            PressedPlay();
+        }
     }
 }
